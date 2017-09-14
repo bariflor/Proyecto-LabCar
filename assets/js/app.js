0c09c8a111a6e794721f9e7e41620817a2d050a5
bariflor <barihuamachuco@gmail.com>
2017-09-14 12:17:48 -0500

    Maquetacion al 50%

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+function initMap() {
+	//creando un nuevo mapa
+	var laboratoriaLima = {lat: -12.1191427, lng: -77.0349046};
+	var map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 5,
+		center: laboratoriaLima,
+		// mapTypeControl: false,// los 3 se agrego-veremos si hay cambio
+		// zoomControl: false,
+		// streetViewControl: false
+	});
+	var markadorLaboratoria = new google.maps.Marker({
+			position: laboratoriaLima,
+			map: map
+	});
+}
